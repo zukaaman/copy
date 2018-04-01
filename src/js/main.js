d8ea31f579545a828e76f94c1b53fb68eaaa5d6c
Konstantin <zukaaman@gmail.com>
2018-04-01 20:54:54 +0400

    first

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/*
+ * Custom
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //= partials/top-header-search.js
+});
---
+++ src/js/partials/top-header-search.js
@@ -0,0 +1,7 @@
+var label = document.querySelector(".search-input-label")
+    body = document.querySelector("body");
+
+label.addEventListener("click", function() {
+  label.classList.add("active");
+});
+
